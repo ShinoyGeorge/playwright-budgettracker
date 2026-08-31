9c437ade4c096ec2136694285055a1e6d1bfaa85
Shinoy George <shinoy.george1987@gmail.com>
2026-08-31 13:08:56 -0400

    added package-lock

--- src/tests/login.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('to have title', {tag: '@12456'}, async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  await expect(page).toHaveTitle(/Two Jars — Budget Tracker/);
-});
